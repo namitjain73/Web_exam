90b37c6a128beccdc1c43c23bfdd455ca5b230ad
angermaster11 <angermaster11@gmail.com>
2025-12-04 11:27:22 +0530

    df

---
+++ backend/routes/auth.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  register,
+  login,
+  refreshAccessToken,
+  me
+} from "../controllers/auth.js";
+
+// import { requireAuth } from "../middleware/requireAuth.js";
+
+const router = express.Router();
+
+// REGISTER
+router.post("/register", register);
+
+// LOGIN
+router.post("/login", login);
+
+// REFRESH TOKEN
+router.post("/refresh-token", refreshAccessToken);
+
+// CURRENT USER
+// router.get("/me", requireAuth, me);
+
+export default router;
